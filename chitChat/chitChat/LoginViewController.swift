a24aa38d0a01d86bfecd064ea0cc685141b352c5
Emmet Susslin <esusslin@gmail.com>
2016-07-08 14:28:49 -0700

    login repetition bug fixed

---
+++ chitChat/chitChat/LoginViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LoginViewController.swift
+//  chitChat
+//
+//  Created by Emmet Susslin on 7/8/16.
+//  Copyright © 2016 Emmet Susslin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    let backendless = Backendless.sharedInstance()
+    
+    var email: String?
+    var password: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: IBAction
+
+    @IBAction func loginBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            
+            self.email = emailTextField.text
+            self.password = passwordTextField.text
+            
+            // login user
+            loginUser(email!, password: password!)
+            
+        } else {
+            // show an error to user
+        }
+    }
+    
+    func loginUser(email: String, password: String) {
+        
+        backendless.userService.login(email, password: password, response: { (user : BackendlessUser!) -> Void in
+            
+            self.passwordTextField.text = ""
+            self.emailTextField.text = ""
+            
+            //here segue to recentsViewController
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ChatVC") as! UITabBarController
+            vc.selectedIndex = 0
+            
+            
+            self.presentViewController(vc, animated: true, completion: nil)
+            
+        }) { (fault : Fault!) -> Void in
+            print("Failed to login user: \(fault)")
+        }
+        
+    }
+
+}
---
+++ chitChat/chitChat/RegisterViewController.swift
@@ -0,0 +1,94 @@
+//
+//  RegisterViewController.swift
+//  chitChat
+//
+//  Created by Emmet Susslin on 7/8/16.
+//  Copyright © 2016 Emmet Susslin. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var backendless = Backendless.sharedInstance()
+    
+    var newUser: BackendlessUser?
+    var email: String?
+    var username: String?
+    var password: String?
+    var avatarImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newUser = BackendlessUser()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: IBActions
+
+    @IBAction func registerButtonPressed(sender: UIButton) {
+        
+        if emailTextField.text != "" && usernameTextField.text != "" {
+            
+            email = emailTextField.text
+            username = usernameTextField.text
+            password = passwordTextField.text
+            
+            register(self.email!, username: self.username!, password: self.password!, avatarImage: self.avatarImage)
+        } else {
+            // warning to user
+        }
+        
+    }
+    
+    func register(email: String, username: String, password: String, avatarImage: UIImage?) {
+        
+        if avatarImage == nil {
+            newUser!.setProperty("Avatar", object: "")
+        }
+        
+        newUser!.email = email
+        newUser!.name = username
+        newUser!.password = password
+        
+        backendless.userService.registering(newUser, response: { (registeredUser : BackendlessUser!) ->
+            Void in
+            
+            //login new user
+            self.loginUser(email, username: username, password: password)
+            
+            self.usernameTextField.text = ""
+            self.passwordTextField.text = ""
+            self.emailTextField.text = ""
+            
+        }) { (fault : Fault!) -> Void in
+            print("Server reported an error, new user registration failed: \(fault)")
+        }
+        
+    }
+    
+    func loginUser(email: String, username: String, password: String) {
+        
+        backendless.userService.login(email, password: password, response: { (user : BackendlessUser!) -> Void in
+            
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ChatVC") as! UITabBarController
+            vc.selectedIndex = 0
+            
+            self.presentViewController(vc, animated: true, completion: nil)
+            
+        }) { (fault : Fault!) in
+            print("Server reported an error: \(fault)")
+        }
+    }
+
+}
