ac7765bd5af74199900b08b7e69555bb1ca1ed40
Emmet Susslin <esusslin@gmail.com>
2016-07-08 16:55:48 -0700

    table view cell complete

---
+++ chitChat/chitChat/RecentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecentViewController.swift
+//  chitChat
+//
+//  Created by Emmet Susslin on 7/8/16.
+//  Copyright © 2016 Emmet Susslin. All rights reserved.
+//
+
+import UIKit
+
+class RecentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - IBActions
+    
+    @IBAction func startNewChatBarButtonItemPressed(sender: AnyObject) {
+    }
+
+
+}
