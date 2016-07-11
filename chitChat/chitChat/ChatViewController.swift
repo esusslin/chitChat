f1136cc1f843588d093cbf022ac0c7a3f65de222
Emmet Susslin <esusslin@gmail.com>
2016-07-11 13:54:29 -0700

    outgoing messages pending

--- chitChat/chitChat/ChatViewController.swift
+++ chitChat/chitChat/ChatViewController.swift
@@ -47,7 +47,82 @@ class ChatViewController: JSQMessagesViewController {
     
     //MARK: JSQMessages dataSource functions
     
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+
+        let cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath) as! JSQMessagesCollectionViewCell
+        
+        let data = messages[indexPath.row]
+        
+        if data.senderId == currentUser.objectId {
+            cell.textView?.textColor = UIColor.whiteColor()
+        } else {
+            cell.textView?.textColor = UIColor.blackColor()
+        }
+        
+        return cell
+    }
     
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        
+        let data = messages[indexPath.row]
+        
+        return data
+        
+        
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
     
 
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        
+        let data = messages[indexPath.row]
+        
+        if data.senderId ==  currentUser.objectId {
+            return outgoingBubble
+        } else {
+            return incomingBubble
+        }
+        
+    }
+    
+    //MARK: JSQMessages Delegate function
+    
+    override func didPressSendButton(sender: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
+        
+        if text != "" {
+            print("send message button pressed")
+            //send message
+        }
+    }
+    
+    override func didPressAccessoryButton(sender: UIButton!) {
+        print("accessory button pressed")
+    }
+    
+    //MARK: send Message
+    
+    func sendMessage(text: String?, date: NSDate, picture: UIImage?, location: String?) {
+        
+        //if text message
+        
+        if let text = text {
+            
+        }
+        
+        //if pic 
+        
+        if let pic = picture {
+            
+        }
+        
+        //if location
+        
+        if let loc = location {
+            
+        }
+        
+    }
 }
